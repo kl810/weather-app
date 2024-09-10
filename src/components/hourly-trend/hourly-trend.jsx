e42196951dd613ef573a2294b2705e9b3fa67e9b
Kye-Lee <kyelee.low@gmail.com>
2024-09-10 16:37:49 +0100

    added humidity chart and windspeed chart, added Tab component so when a temperature, humidity or windspeed tab is clicked - link to the corresponding chart will be displayed

--- src/components/hourly-trend/hourly-trend.jsx
+++ src/components/hourly-trend/hourly-trend.jsx
@@ -1,124 +1,41 @@
 import './hourly-trend.css';
 import { Link, Routes, Route } from 'react-router-dom';
 import { format, fromUnixTime } from 'date-fns';
-import {Chart, CategoryScale, LinearScale, PointElement, LineElement } from 'chart.js'
-import { Line } from 'react-chartjs-2';
-import { transparentize, CHART_COLORS} from '../../helpers/charts';
-import Tab from './tab';
-
-
-Chart.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement
-)
-
+import { TabTemp, TabHumidity, TabWindSpeed } from './tab';
 
 
 function HourlyTrend ({hourlyWeather}) {
 
     const hourlyWeatherLables = hourlyWeather && hourlyWeather.map(hour => format(fromUnixTime(hour.dt), 'HH:mm'))
-    console.log(hourlyWeatherLables && hourlyWeatherLables.slice(0,13))
+    // console.log(hourlyWeatherLables && hourlyWeatherLables.slice(0,13))
     const hourlyTemperatures = hourlyWeather && hourlyWeather.map(hour => hour.temp)
-    console.log(hourlyTemperatures && hourlyTemperatures.slice(0,13))
+    const hourlyHumidity = hourlyWeather && hourlyWeather.map(hour => hour.humidity)
+    const hourlyWindSpeed = hourlyWeather && hourlyWeather.map(hour => hour.wind_speed)
+    // console.log(hourlyWindSpeed && hourlyWindSpeed.slice(0,13))
 
     return (
-        <>
+        <div>
             <div className="links nav nav-tabs">
 
                 <li className="nav-item">
                     <Link to={`/`} className="link nav-link">Temperature</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to={`/rainfall`} className="link nav-link">Rainfall</Link>
+                    <Link to={`/humidity`} className="link nav-link">Humidity</Link>
                 </li>
                 <li className="nav-item">
-                    <Link to={`/humidity`} className="link nav-link">Humidity</Link>    
+                    <Link to={`/wind`} className="link nav-link">Wind</Link>    
                 </li>
                 
             </div>
             <div className="tabs">
                 <Routes>
-                    <Route path={`/`} element={<Tab name="temperature" />} />
-                    <Route path={`/rainfall`} element={<Tab name="rainfall" />} />
-                    <Route path={`/humidity`} element={<Tab name="humidity" />} />
+                    <Route path={`/`} element={<TabTemp hourlyWeatherLables={hourlyWeatherLables} hourlyTemperatures={hourlyTemperatures}/>} />
+                    <Route path={`/humidity`} element={<TabHumidity hourlyWeatherLables={hourlyWeatherLables} hourlyHumidity={hourlyHumidity} />} />
+                    <Route path={`/wind`} element={<TabWindSpeed hourlyWeatherLables={hourlyWeatherLables} hourlyWindSpeed={hourlyWindSpeed} />} /> 
                 </Routes>
             </div>
-
-            <div style={{height: '100px'}}>
-                <Line 
-                    data={
-                        {
-                            datasets: [
-                                {
-                                    data: hourlyTemperatures && hourlyTemperatures.slice(0,13),
-                                    borderColor: CHART_COLORS.red,
-                                    backgroundColor: transparentize(CHART_COLORS.red, 0.5),
-                                    pointStyle: 'circle',
-                                    pointRadius: 10,
-                                    pointHoverRadius: 15
-                                }
-                            ],
-                            labels: hourlyWeatherLables && hourlyWeatherLables.slice(0,13)
-                        }
-                    } 
-                    options={
-
-                        {
-                            // actions: {
-                            //     name: 'pointStyle: circle (default)',
-                            //     handler: (chart) => {
-                            //       chart.data.datasets.forEach(dataset => {
-                            //         dataset.pointStyle = 'circle';
-                            //       });
-                            //       chart.update();
-                            //     }
-                            //   },
-
-
-                            maintainAspectRatio: false,
-                            scales: {
-                                x: {
-                                  grid: {
-                                    display: false
-                                  },
-                                  border: {
-                                    display: false
-                                  }
-                                },
-                                y: {
-                                    ticks: {
-                                        stepSize: 2
-                                     },
-                                  grid: {
-                                    display: false
-                                  },
-                                  border: {
-                                    display: false
-                                  }
-                                }
-                              }
-                        }
-                    }
-                />
-            </div>
-        </>
-    )
-}
+        </div>
+    )}
 
 export default HourlyTrend;
-
-
-
-                {/* <li className="nav-item">
-                    <a className="nav-link active" href="#">Temperature</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Rainfall</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Humidity</a>
-                </li> */}
-
-
--- src/components/hourly-trend/tab.jsx
+++ src/components/hourly-trend/tab.jsx
@@ -1,6 +1,170 @@
+import {Chart, CategoryScale, LinearScale, PointElement, LineElement, BarElement } from 'chart.js'
+import { Line, Bar } from 'react-chartjs-2';
+import { transparentize, CHART_COLORS} from '../../helpers/charts';
+
+Chart.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    BarElement
+)
 
 
+export function TabTemp({hourlyWeatherLables, hourlyTemperatures}) {
 
-export default function Tab({name}) {
-    return <p>Hello {name}</p>
+    return (
+        <div style={{height: '100px'}}>                
+            <Line 
+                data={
+                    {
+                        datasets: [
+                            {
+                                data: hourlyTemperatures && hourlyTemperatures.slice(0,13),
+                                borderColor: CHART_COLORS.red,
+                                backgroundColor: transparentize(CHART_COLORS.red, 0.5),
+                                pointStyle: 'circle',
+                                pointRadius: 5,
+                                pointHoverRadius: 10
+                            }
+                        ],
+                        labels: hourlyWeatherLables && hourlyWeatherLables.slice(0,13)
+                    }
+                } 
+                options={
+
+                    {
+
+                        maintainAspectRatio: false,
+                        scales: {
+                            x: {
+                            grid: {
+                                display: false
+                            },
+                            border: {
+                                display: false
+                            }
+                            },
+                            y: {
+                                ticks: {
+                                    stepSize: 2
+                                },
+                            grid: {
+                                display: false
+                            },
+                            border: {
+                                display: false
+                            }
+                            }
+                        }
+                    }
+                }
+            />
+        </div>
+    )
+};
+
+export function TabHumidity({hourlyWeatherLables, hourlyHumidity}) {
+
+    return (
+    <div style={{height: '100px'}}>
+        <Bar 
+            data={
+                {
+                    datasets: [
+                        {
+                            data: hourlyHumidity && hourlyHumidity.slice(0,13),
+                            borderColor: CHART_COLORS.blue,
+                            backgroundColor: transparentize(CHART_COLORS.blue, 0.5),
+                            borderWidth: 2,
+                            borderRadius: 5,
+                            // borderSkipped: false,
+                        }
+                    ],
+                    labels: hourlyWeatherLables && hourlyWeatherLables.slice(0,13)
+                }
+            } 
+            options={
+
+                {
+
+                    maintainAspectRatio: false,
+                    scales: {
+                        x: {
+                            grid: {
+                            display: false
+                            },
+                            border: {
+                            display: false
+                            }
+                        },
+                        y: {
+                            ticks: {
+                                stepSize: 2
+                                },
+                            grid: {
+                            display: false
+                            },
+                            border: {
+                            display: false
+                            }
+                        }
+                        }
+                }
+            }
+        />
+    </div>
+)};
+
+
+export function TabWindSpeed({hourlyWeatherLables, hourlyWindSpeed}) {
+    return (
+        <div style={{height: '100px'}}>
+            <Bar 
+                data={
+                    {
+                        datasets: [
+                            {
+                                data: hourlyWindSpeed && hourlyWindSpeed.slice(0,13),
+                                borderColor: CHART_COLORS.blue,
+                                backgroundColor: transparentize(CHART_COLORS.blue, 0.5),
+                                borderWidth: 2,
+                                borderRadius: 5,
+                                // borderSkipped: false,
+                            }
+                        ],
+                        labels: hourlyWeatherLables && hourlyWeatherLables.slice(0,13)
+                    }
+                } 
+                options={
+
+                    {
+
+                        maintainAspectRatio: false,
+                        scales: {
+                            x: {
+                                grid: {
+                                display: false
+                                },
+                                border: {
+                                display: false
+                                }
+                            },
+                            y: {
+                                ticks: {
+                                    stepSize: 2
+                                    },
+                                grid: {
+                                display: false
+                                },
+                                border: {
+                                display: false
+                                }
+                            }
+                            }
+                    }
+                }
+            />
+        </div>
+    )
 }
